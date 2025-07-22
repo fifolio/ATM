3622219d1fbc3fd4e47e68575f441f3dcf24a2f5
fifolio <firasdabbabi@gmail.com>
2025-07-22 10:12:38 +0100

    Refactor: Stop tracking src/algos

--- src/algos/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as runMarketInsights } from './market_insights/market_insights';
-export { default as PRUxMRPU_handler } from './lib/RPUxMRPU_handler';
--- src/algos/market_insights/market_insights.ts
+++
@@ -1,5 +0,0 @@
-function runMarketInsights() {
-    return console.log("Running market insights...");
-}
-
-export default runMarketInsights;
